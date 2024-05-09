1f6045a437902adb84125cf3a822cdb2283b996a
Ladalo-Daniel <balaladalo@gmail.com>
2024-05-09 19:55:55 +0100

    landing page updated

--- src/components/home/HeroSearch.jsx
+++ src/components/home/HeroSearch.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const HeroSearch = () => {
   return (
-        <div className=' w-[94%] max-lg:w-[94%] max-lg:mx-4 ml-16 searc blur-6 text-payWhite bg-orange-400 opacity-95  rounded-md absolute max-sm:bottom-[1.5rem] max-sm:px-4 max-md:bottom-[2rem] bottom-[0.5rem]  '>
+        <div className=' w-[94%] max-lg:w-[94%] max-lg:mx-4 ml-16 searc blur-6 text-payWhite bg-orange-400 opacity-95  rounded-md absolute max-sm:bottom-[1.5rem] max-sm:px-4 max-md:bottom-[2rem] bottom-[3.5rem]  '>
             <div
-            className=" w-full px-4 mx-auto mt-12 my-5">
-            <div className=" flex flex-row gap-2">
+            className=" w-full gap-2 px-4 mx-auto mt-10 my-5">
+            <div className=" flex flex-row gap-2 mb-4">
                <div className="w-full py-3 pl-4 pr-4 text-gray-500 border rounded-sm outline-none bg-gray-50 focus:bg-white focus:border-blue-600">
                 <p><strong>Reliability:</strong> You can trust us for genuine products, transparent reviews, and reliable customer service. </p>
                </div>
@@ -15,7 +15,7 @@ const HeroSearch = () => {
                </div>
                
             </div>
-            
+
             <Link href={"https://alx-portfolio-project-yichelo-gadget-e-commerce-store.vercel.app/"} className=' bg-white text-black py-2 rounded-lg mt-5 px-6 mb-5 hover:opacity-90'>Explore now</Link>
         </div>
         </div>
